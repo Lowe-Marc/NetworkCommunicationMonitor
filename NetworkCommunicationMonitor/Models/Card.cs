7f143d46fc0ff07b817574cf7bdd5f1b1544e549
Lowe-Marc <Marcus.S.Lowe@gmail.com>
2018-02-27 20:00:44 -0600

    The tables in the Card, Relay and Store tabs will now draw from the database.

--- NetworkCommunicationMonitor/Models/Card.cs
+++ NetworkCommunicationMonitor/Models/Card.cs
@@ -15,10 +15,42 @@ namespace NetworkCommunicationMonitor.Models
     {
 
         public string cardNumber;
+        public string firstName;
+        public string lastName;
+        public int expirationMonth;
+        public int expirationYear;
+        public string accountNumber;
 
         public static List<Card> getCards()
         {
             List<Card> cards = new List<Card>();
+
+            var cn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            using (cn)
+            {
+                DataTable questionTable = new DataTable();
+                DataRowCollection rows;
+                string _sql = @"SELECT card_id, card_firstname, card_lastname, card_expirationMonth, card_expirationYear, account_id FROM Card";
+                var cmd = new SqlCommand(_sql, cn);
+
+                cn.Open();
+
+                questionTable.Load(cmd.ExecuteReader());
+                rows = questionTable.Rows;
+
+                foreach (DataRow row in rows)
+                {
+                    Card tempCard = new Card();
+                    tempCard.cardNumber = Convert.ToString(row["card_id"]);
+                    tempCard.firstName = Convert.ToString(row["card_firstname"]);
+                    tempCard.lastName = Convert.ToString(row["card_lastname"]);
+                    tempCard.expirationMonth = Convert.ToInt32(row["card_expirationMonth"]);
+                    tempCard.expirationYear = Convert.ToInt32(row["card_expirationYear"]);
+                    tempCard.accountNumber = Convert.ToString(row["account_id"]);
+                    cards.Add(tempCard);
+                }
+            }
+
             return cards;
         }
 
--- NetworkCommunicationMonitor/Models/Store.cs
+++ NetworkCommunicationMonitor/Models/Store.cs
@@ -14,9 +14,35 @@ namespace NetworkCommunicationMonitor.Models
     public class Store
     {
 
+        public string ipAddress;
+        public string name;
+
         public static List<Store> getStores()
         {
             List<Store> stores = new List<Store>();
+
+            var cn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+            using (cn)
+            {
+                DataTable questionTable = new DataTable();
+                DataRowCollection rows;
+                string _sql = @"SELECT store_id, store_name FROM Store";
+                var cmd = new SqlCommand(_sql, cn);
+
+                cn.Open();
+
+                questionTable.Load(cmd.ExecuteReader());
+                rows = questionTable.Rows;
+
+                foreach (DataRow row in rows)
+                {
+                    Store tempStore = new Store();
+                    tempStore.ipAddress = Convert.ToString(row["store_id"]);
+                    tempStore.name = Convert.ToString(row["store_name"]);
+                    stores.Add(tempStore);
+                }
+            }
+
             return stores;
         }
 
